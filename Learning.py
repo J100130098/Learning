4f3e6cc80a0cc3ab399c4dbf874a0bf752d88f94
100130098 <jjhill080103@gmail.com>
2019-09-30 09:02:01 -0500

    Bruh

--- Learning.py
+++ Learning.py
@@ -1,2 +1,4 @@
+# This is to comment on the code...
 print("Change the world, my final message, goodbye")
-print("I am going to sleep")
+print("I am going to sleep", 2+34/2 % 69)
+print(3+2+1-5+4%2-1/4+6)
