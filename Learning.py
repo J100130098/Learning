2733fdd2da697a5761c8ced9055b907b2e4e4c40
100130098 <jjhill080103@gmail.com>
2019-10-01 08:29:02 -0500

    mega bruh moment

--- Learning.py
+++ Learning.py
@@ -1,12 +1,14 @@
 # This is to comment on the code...
-#
+# The line below is to simply print out text in the python program
 print("Change da world, my final message, goodbye")
 # math skills demo
+# this
 print("the number of protons in hydrogen", 1)
 print("random Math", 15+30/6)
 print("even more math", 100-25*13)
 #
 print("I am going to sleep", 2+34/2 % 69)
 #
-print(3+2+1-5+4%2-1/4+6)
-print("using math to find super big numbers", )
+print("The length of about half of a 12 inch ruler",3+2+1-5+4%2-1/4+6)
+#
+print("using math to find super big numbers because I don't really have many things to calculate",(346*15**2)**16/(2%55) )
