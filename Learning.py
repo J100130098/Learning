bdc6f566b6ab441fd8741d96defec70029c00364
100130098 <jjhill080103@gmail.com>
2019-10-01 08:24:12 -0500

    mega bruh moment

--- Learning.py
+++ Learning.py
@@ -1,4 +1,12 @@
 # This is to comment on the code...
-print("Change the world, my final message, goodbye")
+#
+print("Change da world, my final message, goodbye")
+# math skills demo
+print("the number of protons in hydrogen", 1)
+print("random Math", 15+30/6)
+print("even more math", 100-25*13)
+#
 print("I am going to sleep", 2+34/2 % 69)
+#
 print(3+2+1-5+4%2-1/4+6)
+print("using math to find super big numbers", )
