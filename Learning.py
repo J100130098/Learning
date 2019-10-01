df495841ef78346c8e8393cfc5c5617f4c9a4c2c
100130098 <jjhill080103@gmail.com>
2019-10-01 08:57:34 -0500

    created floating point numbers

--- Learning.py
+++ Learning.py
@@ -5,13 +5,13 @@ print("Change da world, my final message, goodbye")
 # this is to display text and it will display a number as well, in this case being 1
 print("the number of protons in hydrogen", 1)
 # the line below allows you to create math, this one is adding 15 to 30 divided by 6
-print("random Math", 15+30/6)
+print("random Math", 15.0+30.0/6.0)
 # the line below is also doing math, this one is subtracting 100 from 25 times 13
-print("even more math", 100-25*13)
+print("even more math", 100.0-25.0*13.0)
 # the line below is giving the remainder of 2+34/2 when the answer is divided by 2
-print("I am going to sleep", 2+34/2 % 2)
+print("I am going to sleep", 2.00+34.0000/2.00 % 2.0)
 # The line below is doing the same as above but with a different equation (the remainder of (3+2+1-5+4)/(2-1/4+6)
-print("The length of about half of a 12 inch ruler", 3+2+1-5+4 % 2-1/4+6)
+print("The length of about half of a 12 inch ruler", 3.0+2.0+1.0-5.0+4.0 % 2.0-1.0/4.0+6.0)
 # The last line is calculating a large number because I don't have anything I need to calculate at the moment
-print("using math to find super big numbers because I don't really have many things to calculate",(346*15**2)**16/(2 % 55))
+print("using math to find super big numbers because I don't really have many things to calculate",(346.00*15.0**2.0)**16.0/(2.0 % 55.0))
 # a floating point number is a representation of real numbers as an approximation to create a trade off between range and precision. they can be incredibly small or incredibly large numbers.
