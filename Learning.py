790f96d355b86d8445ab2b5a48211e10211ce897
100130098 <jjhill080103@gmail.com>
2019-10-01 08:45:30 -0500

    mega bruh moment

--- Learning.py
+++ Learning.py
@@ -2,12 +2,14 @@
 # The line below is to simply print out text in the python program
 print("Change da world, my final message, goodbye")
 # math skills demo
-# this
+# this is to display text and it will display a number as well, in this case being 1
 print("the number of protons in hydrogen", 1)
+# the line below allows you to create math, this one is adding 15 to 30 divided by 6
 print("random Math", 15+30/6)
+# the line below is also doing math, this one is subtracting 100 from 25 times 13
 print("even more math", 100-25*13)
-#
-print("I am going to sleep", 2+34/2 % 69)
+# the line below is giving the remainder of 2+34/2 when the answer is divided by 2
+print("I am going to sleep", 2+34/2 % 2)
 #
 print("The length of about half of a 12 inch ruler",3+2+1-5+4%2-1/4+6)
 #
